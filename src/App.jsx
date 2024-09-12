72af68fef82d53af12ecc403f4105c0d7a32d1da
hardik0018 <141490030+hardik0018@users.noreply.github.com>
2024-09-12 10:55:05 +0530

    s

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Map from "./components/Map";
 import { io } from "socket.io-client";
 
 function App() {
-  const socket = useMemo(() => io("http://localhost:4000"));
+  const socket = useMemo(() => io(import.meta.env.VITE_SERVER_HOST));
   const [AllUserLoacation, setAllUserLoacation] = useState(false);
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
